a1116162611adbf7bd9163f14581911500e9fe80
Corvince <13568919+Corvince@users.noreply.github.com>
2020-02-11 20:45:15 +0100

    Fix tornado errors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import re
 from setuptools import setup, find_packages
 from codecs import open
 
-requires = ["mesa", "geopandas", "libpysal", "rtree"]
+requires = ["mesa >= 0.8.6", "geopandas", "libpysal", "rtree"]
 
 version = ""
 with open("mesa_geo/__init__.py", "r") as fd:
