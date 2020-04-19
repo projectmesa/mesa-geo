3952b92abb3bed564d27cef877b7653ca87eebea
glicerico <suarezandres@gmail.com>
2020-04-19 13:31:18 -0600

    Continue building basic model

--- examples/SimpleToronto/model.py
+++ examples/SimpleToronto/model.py
@@ -1,9 +1,28 @@
 from mesa.datacollection import DataCollector
-from mesa import Model
+from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa_geo.geoagent import GeoAgent, AgentCreator
 from mesa_geo import GeoSpace
 import random
+
+
+class PersonAgent(GeoAgent):
+    """Person Agent."""
+    def __init__(self, unique_id, model, agent_type="susceptible"):
+        """ Create a new Person agent.
+
+        Args:
+            unique_id: Unique identifier for the agent
+            agent_type: Indicator if agent is infected ("infected" or "susceptible")
+            """
+        super().__init__(unique_id, model, "Point")
+        self.infected = 0
+        self.agent_type = agent_type
+
+    def step(self):
+        # The agent's step will go here.
+        self.infected += 1
+        print(f"AGENT COUNTER: {self.infected}")
 
 
 class NeighbourhoodAgent(GeoAgent):
@@ -69,6 +88,11 @@ class InfectedModel(Model):
         AC = AgentCreator(NeighbourhoodAgent, {"model": self})
         agents = AC.from_file("TorontoNeighbourhoods.geojson")
         self.grid.add_agents(agents)
+
+        # Create a person Agent, add it
+        person = PersonAgent('test', self)
+        self.grid.add_agents(person)
+        self.schedule.add(person)
 
         # Set up agents
         for agent in agents:
