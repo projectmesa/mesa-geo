6ec11645ee15b0e84b9e613881fd1dadde0e9ef4
glicerico <suarezandres@gmail.com>
2020-04-24 15:57:03 +0800

    Update slider parameter

--- examples/SimpleToronto/server.py
+++ examples/SimpleToronto/server.py
@@ -18,7 +18,7 @@ class InfectedText(TextElement):
 
 
 model_params = {
-    "density": UserSettableParameter("slider", "Agent density", 0.6, 0.1, 1.0, 0.1),
+    "pop_size": UserSettableParameter("slider", "Population size", 10, 10, 100, 10),
     "minority_pc": UserSettableParameter(
         "slider", "Fraction minority", 0.2, 0.00, 1.0, 0.05
     ),
@@ -31,10 +31,8 @@ def infected_draw(agent):
     """
     portrayal = dict()
     if isinstance(agent, PersonAgent):
-        portrayal["Shape"] = 'Circle'
+        # portrayal["Shape"] = 'Triangle'
         portrayal["radius"] = '2'
-        portrayal["x"] = '0'
-        portrayal["y"] = '0'
     if agent.atype in ['hotspot', 'infected']:
         portrayal["color"] = "Red"
     else:
@@ -42,10 +40,8 @@ def infected_draw(agent):
     return portrayal
 
 
-# coords = [43.547899, -96.735894]  # Sioux Falls
-coords = [43.741667, -79.373333]  # Toronto
 infected_text = InfectedText()
-map_element = MapModule(infected_draw, coords, 10, 500, 500)
+map_element = MapModule(infected_draw, InfectedModel.MAP_COORDS, 10, 500, 500)
 infected_chart = ChartModule([{"Label": "infected", "Color": "Black"}])
 server = ModularServer(
     InfectedModel, [map_element, infected_text, infected_chart], "Infected", model_params
