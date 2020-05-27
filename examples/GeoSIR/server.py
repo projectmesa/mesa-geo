9715ff88a35ce6646a101d31f7eb29a026ee3283
Andres Suarez <suarezandres@gmail.com>
2020-05-28 04:40:30 +0800

    SIR example (#23)
    * Add gitignore
    
    * Add Toronto files
    
    * Rename variables
    
    * Continue building basic model
    
    * Start with a base running model
    
    * Start with a base running model
    
    * Add one point agent; can reproduce issue#17
    
    * Create agent from string
    
    * Implement move person agent
    
    * Implement move person agent
    
    * Try update both areas and point agents
    
    * Try update region colors
    
    * Achieve display of Point and Polygon
    
    * Parametrize city coordinates
    
    * Add simplified Toronto data
    
    * Update region color accoriding to people
    
    * Add test second agent
    
    * Delete person geojson file
    
    * Update slider parameter
    
    * Generate agent population
    
    * Move agents around center
    
    * Improve legibility in agent creation
    
    * Set initial infected as parameter
    
    * Adjust simplified neighbourhoods
    
    * Parametrize values
    
    * Implement very basic SIR
    
    * Implement data colletion and plot
    
    * Change visualuzation wording
    
    * Add documentation
    
    * Add minor documentation
    
    * Write README
    
    * Rename file
    
    * Make initial agent range class variable
    
    * Color hotpots before start
    
    * Change to BaseScheduler for proper hotspot color
    
    * Change folder name
    
    * Add Toronto data reference
    
    * Remove folder with old name
    
    * Add running instruction to README
    
    * Distribute person agents in neighbourhoods
    
    * Link agent initial spread with region bounds
    
    * Remove unused import
    
    * PERF: don't iterate over all neighbors
    
    * dont profile by default
    
    * apply black formatting
    
    Co-authored-by: Corvince <13568919+Corvince@users.noreply.github.com>

---
+++ examples/GeoSIR/server.py
@@ -0,0 +1,65 @@
+from mesa_geo.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
+from model import InfectedModel, PersonAgent
+from mesa_geo.visualization.MapModule import MapModule
+
+
+class InfectedText(TextElement):
+    """
+    Display a text count of how many steps have been taken
+    """
+
+    def __init__(self):
+        pass
+
+    def render(self, model):
+        return "Steps: " + str(model.steps)
+
+
+model_params = {
+    "pop_size": UserSettableParameter("slider", "Population size", 30, 10, 100, 10),
+    "init_infected": UserSettableParameter(
+        "slider", "Fraction initial infection", 0.2, 0.00, 1.0, 0.05
+    ),
+    "exposure_distance": UserSettableParameter(
+        "slider", "Exposure distance", 500, 100, 1000, 100
+    ),
+}
+
+
+def infected_draw(agent):
+    """
+    Portrayal Method for canvas
+    """
+    portrayal = dict()
+    if isinstance(agent, PersonAgent):
+        portrayal["radius"] = "2"
+    if agent.atype in ["hotspot", "infected"]:
+        portrayal["color"] = "Red"
+    elif agent.atype in ["safe", "susceptible"]:
+        portrayal["color"] = "Green"
+    elif agent.atype in ["recovered"]:
+        portrayal["color"] = "Blue"
+    elif agent.atype in ["dead"]:
+        portrayal["color"] = "Black"
+    return portrayal
+
+
+infected_text = InfectedText()
+map_element = MapModule(infected_draw, InfectedModel.MAP_COORDS, 10, 500, 500)
+infected_chart = ChartModule(
+    [
+        {"Label": "infected", "Color": "Red"},
+        {"Label": "susceptible", "Color": "Green"},
+        {"Label": "recovered", "Color": "Blue"},
+        {"Label": "dead", "Color": "Black"},
+    ]
+)
+server = ModularServer(
+    InfectedModel,
+    [map_element, infected_text, infected_chart],
+    "Basic agent-based SIR model",
+    model_params,
+)
+server.launch()
