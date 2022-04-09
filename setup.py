62735d642878233149aa94d1319aaa8d44678888
Wang Boyu <boyu.wby@gmail.com>
2022-04-08 22:21:27 -0400

    create setup.cfg and pyproject.toml from setup.py

--- setup.py
+++ setup.py
@@ -5,26 +5,22 @@ import pkgutil
 import re
 import shutil
 import sys
-from codecs import open
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 
-requires = ["mesa >= 0.8.6", "geopandas", "libpysal", "rtree"]
 
-version = ""
-with open("mesa_geo/__init__.py", "r") as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
-    ).group(1)
-
-with open("README.md", "r") as f:
-    readme = f.read()
+def get_version_from_package() -> str:
+    with open("mesa_geo/__init__.py", "r") as fd:
+        version = re.search(
+            r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+        ).group(1)
+    return version
 
 
 class DevelopCommand(develop):
-    """Pre-installation for development mode."""
+    """Installation for development mode."""
 
     def run(self):
         get_mesa_viz_files()
@@ -32,7 +28,7 @@ class DevelopCommand(develop):
 
 
 class InstallCommand(install):
-    """Pre-installation for installation mode."""
+    """Installation for installation mode."""
 
     def run(self):
         get_mesa_viz_files()
@@ -64,42 +60,12 @@ def get_mesa_templates(package, template_dir):
             )
 
 
-setup(
-    name="mesa-geo",
-    version=version,
-    description="Agent-based modeling (ABM) in Python 3+",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Project GeoMesa Team",
-    author_email="",
-    url="https://github.com/projectmesa/mesa-geo",
-    packages=find_packages(),
-    package_data={
-        "mesa_geo": [
-            "visualization/templates/*.html",
-            "visualization/templates/css/*",
-            "visualization/templates/js/*",
-            "visualization/templates/external/**/**/*",
-        ],
-    },
-    include_package_data=True,
-    install_requires=requires,
-    keywords="agent based modeling model ABM simulation multi-agent",
-    license="Apache 2.0",
-    zip_safe=False,
-    classifiers=[
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Artificial Life",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Natural Language :: English",
-    ],
-    cmdclass={
-        "develop": DevelopCommand,
-        "install": InstallCommand,
-    },
-)
+if __name__ == "__main__":
+    setup(
+        name="Mesa-Geo",
+        version=get_version_from_package(),
+        cmdclass={
+            "develop": DevelopCommand,
+            "install": InstallCommand,
+        },
+    )
