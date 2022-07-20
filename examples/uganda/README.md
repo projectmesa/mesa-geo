93e8d602b367bc094660eb1f6758a913086fc461
Wang Boyu <boyu.wby@gmail.com>
2022-07-20 18:08:16 +0800

    add uganda example

---
+++ examples/uganda/README.md
@@ -0,0 +1,14 @@
+Uganda Model
+==============
+
+An implementation of the [Uganda Example](https://github.com/abmgis/abmgis/tree/master/Chapter05-GIS/Models/UgandaExample) in Python, using [Mesa](https://github.com/projectmesa/mesa) and [Mesa-Geo](https://github.com/projectmesa/mesa-geo).
+
+## How to run
+
+To run the model interactively, run `mesa runserver` in this directory. e.g.
+
+```bash
+mesa runserver
+```
+
+Then open your browser to [http://127.0.0.1:8521/](http://127.0.0.1:8521/) and press `Start`.
---
+++ examples/uganda/uganda/server.py
@@ -0,0 +1,47 @@
+import mesa
+from shapely.geometry import Point, Polygon
+
+from mesa_geo.geoagent import GeoAgent
+from mesa_geo.visualization.ModularVisualization import ModularServer
+from mesa_geo.visualization.modules import MapModule
+
+from .model import Uganda
+from .space import UgandaCell
+
+
+class NumAgentsElement(mesa.visualization.TextElement):
+    def __init__(self):
+        super().__init__()
+
+    def render(self, model):
+        return f"Number of Agents: {len(model.space.agents)}"
+
+
+def agent_portrayal(agent):
+    if isinstance(agent, GeoAgent):
+        if isinstance(agent.geometry, Point):
+            return {
+                "stroke": False,
+                "color": "Green",
+                "radius": 2,
+                "fillOpacity": 0.3,
+            }
+        elif isinstance(agent.geometry, Polygon):
+            return {
+                "fillColor": "Blue",
+                "fillOpacity": 1.0,
+            }
+    elif isinstance(agent, UgandaCell):
+        return (agent.population, agent.population, agent.population, 1)
+
+
+geospace_element = MapModule(
+    agent_portrayal,
+    [0.18357442221655898, 32.877823549000034],
+    12.9,
+    map_height=500,
+    map_width=500,
+)
+num_agents_element = NumAgentsElement()
+
+server = ModularServer(Uganda, [geospace_element, num_agents_element], "Uganda Model")
