ef6286a8dc9b5d0de8ba2fb5371d0cd7b475baaf
Tom Pike <tpike3@gmu.edu>
2024-04-07 09:08:51 -0400

    Update __init__.py
    * fix init import

--- mesa_geo/geoexperimental/__init__.py
+++ mesa_geo/geoexperimental/__init__.py
@@ -1,5 +1,4 @@
 from .jupyter_viz import JupyterViz, make_text, Slider  # noqa
-from mesa_geo.geoexperimental import cell_space
 
 
-__all__ = ["JupyterViz", "make_text", "Slider", "cell_space"]
+__all__ = ["JupyterViz", "make_text", "Slider"]
