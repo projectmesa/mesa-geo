9fe214f11a5158b2bd3e789696d5ebf64e3aa6ab
Tom Pike <tpike3@gmu.edu>
2024-05-11 07:14:27 -0400

    remove legacy visualization module

--- mesa_geo/visualization/__init__.py
+++ mesa_geo/visualization/__init__.py
@@ -2,7 +2,4 @@
 from mesa_geo.visualization.geojupyter_viz import GeoJupyterViz
 from mesa_geo.visualization.leaflet_viz import LeafletViz
 
-# Import modules directory
-from mesa_geo.visualization.modules import *  # noqa
-
 __all__ = ["GeoJupyterViz", "LeafletViz"]
