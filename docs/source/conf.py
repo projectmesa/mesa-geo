4149e78c335ca6e913c3ae714f4d555d07130276
Tom Pike <tpike3@gmu.edu>
2024-06-09 14:26:23 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.mathjax",
     "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
-    'jupyterlite_sphinx',
+    "sphinx.ext.viewcode"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
