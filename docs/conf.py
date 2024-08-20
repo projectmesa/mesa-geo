cbde87a3ea17be6a1283e08bcf8dccc13fbbf4fb
Tom Pike <tpike3@gmu.edu>
2024-08-20 06:20:54 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ release = "0.7.1"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "build"]
+exclude_patterns = ["_build"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
